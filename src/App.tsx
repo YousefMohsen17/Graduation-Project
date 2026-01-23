e01203f6e42a8884cc49d9a4d386d09bc23d1995
Yousef Mohsen <ymohsen746@gmail.com>
2026-01-24 00:07:24 +0200

    edit in pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,57 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import LandingPage from "./pages/landing/LandingPage";
 import SignupPage from "./pages/signup/SignupPage";
 import SigninPage from "./pages/signin/SigninPage";
+import {
+
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { Toaster } from "react-hot-toast";
+import Layout from "./components/Layout";
+import HomePage from "./pages/Home/HomePage";
+import CoursesPage from "./pages/courses/CoursesPage";
+import SubjectDetailsPage from "./pages/courses/SubjectDetailsPage";
 
 function App() {
+
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: (
-        <>
-          <LandingPage />
-        </>
-      ),
+
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: (
+            <>
+              <LandingPage />
+            </>
+          ),
+        },
+        {
+          path: "/home",
+          element: (
+            <>
+              <HomePage />
+            </>
+          ),
+        },
+        {
+          path: "/courses",
+          element: (
+            <>
+              <CoursesPage />
+            </>
+          ),
+        },
+        {
+          path: "/courses/:id",
+          element: (
+            <>
+              <SubjectDetailsPage />
+            </>
+          ),
+        },
+      ],
     },
     {
       path: "/sign-up",
@@ -30,10 +71,13 @@ function App() {
       ),
     },
   ]);
+  const queryClient = new QueryClient()
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
       <RouterProvider router={router}></RouterProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/landing/components/Header.tsx
+++ src/pages/landing/components/Header.tsx
@@ -1,16 +1,23 @@
-import { Link } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 import logo from "../../../assets/logo.svg";
 import ButtonLink from "../../../components/ButtonLink";
 import { useState } from "react";
-import { Menu, X } from "lucide-react";
+import { Bell, CircleUserRound, Menu, MessageCircle, X } from "lucide-react";
+import { useAuth } from "../../../lib/queries";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { data: user } = useAuth();
+  const isActive = (path: string) => {
+    return location.pathname === path ? "text-[#0a0e29] font-bold" : "text-[#0a0e29]/70 font-normal";
+  };
 
   return (
     <header className="relative z-50">
       <div className="container mx-auto p-4 flex justify-between items-center mb-10 lg:mb-[100px]">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 cursor-pointer" onClick={() => console.log(user)}>
           <h1 className="text-xl font-[Orbitron] font-semibold">Engipedia</h1>
           <img src={logo} alt="Fire Spirit" />
         </div>
@@ -18,38 +25,64 @@ export default function Header() {
         {/* Desktop Navigation */}
         <nav className="hidden lg:block">
           <ul className="flex gap-4 p-2 ">
-            <Link to="/">
-              <li className="font-bold hover:text-[#0a0e29]/70 transition-colors">Home</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-[#0a0e29]/70 transition-colors">Courses</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-[#0a0e29]/70 transition-colors">Progress</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-[#0a0e29]/70 transition-colors">Community</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-[#0a0e29]/70 transition-colors">About</li>
-            </Link>
+            {user ? (
+              <>
+                <Link to="/home">
+                  <li className={`hover:text-[#0a0e29] transition-colors ${isActive('/home')}`}>Home</li>
+                </Link>
+                <Link to="/courses">
+                  <li className={`hover:text-[#0a0e29] transition-colors ${isActive('/courses')}`}>Courses</li>
+                </Link>
+                <Link to="/">
+                  <li className={`hover:text-[#0a0e29] transition-colors ${isActive('/progress')}`}>Progress</li>
+                </Link>
+                <Link to="/">
+                  <li className={`hover:text-[#0a0e29] transition-colors ${isActive('/community')}`}>Community</li>
+                </Link>
+                <Link to="/">
+                  <li className={`hover:text-[#0a0e29] transition-colors ${isActive('/about')}`}>About</li>
+                </Link>
+              </>
+            ) : null}
           </ul>
         </nav>
 
         {/* Desktop Buttons */}
         <div className="hidden lg:flex gap-10 items-center">
-          <ButtonLink
-            to="/sign-up"
-            children="Sign Up"
-            variant="solid"
-            className="py-6 px-4 h-[54px]"
-          />
-          <ButtonLink
-            to="/sign-in"
-            children="Sign In"
-            variant="outline"
-            className="py-6 px-4 h-[54px]"
-          />
+          {!user && (
+            <>
+              <ButtonLink
+                to="/sign-up"
+                children="Sign Up"
+                variant="solid"
+                className="py-6 px-4 h-[54px]"
+              />
+              <ButtonLink
+                to="/sign-in"
+                children="Sign In"
+                variant="outline"
+                className="py-6 px-4 h-[54px]"
+              />
+            </>
+          )}
+          {user && (
+            <div className="flex justify-between items-center font-medium  text-[#0a0e29]">
+              <div>
+                <CircleUserRound />
+              </div>
+              <div className="ms-[15px] me-[24px]">
+
+                <p className="font-bold">{user.data?.name}</p>
+                <p>{user.data?.email}</p>
+              </div>
+              <div className="ms-[10px]">
+                <Bell />
+              </div>
+              <div className="ms-[10px]">
+                <MessageCircle />
+              </div>
+            </div>
+          )}
         </div>
 
         {/* Mobile Menu Button */}
@@ -65,37 +98,39 @@ export default function Header() {
           <div className="absolute top-full left-0 w-full bg-[#D6DAF5] shadow-lg flex flex-col items-center p-6 gap-6 lg:hidden border-t border-[#0a0e29]/10">
             <nav>
               <ul className="flex flex-col gap-6 text-center text-lg">
-                <Link to="/" onClick={() => setIsMenuOpen(false)}>
-                  <li className="font-bold">Home</li>
-                </Link>
-                <Link to="/" onClick={() => setIsMenuOpen(false)}>
-                  <li>Courses</li>
-                </Link>
-                <Link to="/" onClick={() => setIsMenuOpen(false)}>
-                  <li>Progress</li>
-                </Link>
-                <Link to="/" onClick={() => setIsMenuOpen(false)}>
-                  <li>Community</li>
-                </Link>
-                <Link to="/" onClick={() => setIsMenuOpen(false)}>
-                  <li>About</li>
-                </Link>
+                {user ? (
+                  <>
+                    <Link to="/home" onClick={() => setIsMenuOpen(false)}>
+                      <li className={isActive('/home')}>Home</li>
+                    </Link>
+                    <Link to="/courses" onClick={() => setIsMenuOpen(false)}>
+                      <li className={isActive('/courses')}>Courses</li>
+                    </Link>
+                    {/* Add other mobile links if needed */}
+                  </>
+                ) : (
+                  <Link to="/" onClick={() => setIsMenuOpen(false)}>
+                    <li className={isActive('/')}>Home</li>
+                  </Link>
+                )}
               </ul>
             </nav>
-            <div className="flex flex-col gap-4 w-full max-w-xs">
-              <ButtonLink
-                to="/sign-up"
-                children="Sign Up"
-                variant="solid"
-                className="py-4 px-4 h-[50px] w-full justify-center"
-              />
-              <ButtonLink
-                to="/sign-in"
-                children="Sign In"
-                variant="outline"
-                className="py-4 px-4 h-[50px] w-full justify-center"
-              />
-            </div>
+            {!user && (
+              <div className="flex flex-col gap-4 w-full max-w-xs">
+                <ButtonLink
+                  to="/sign-up"
+                  children="Sign Up"
+                  variant="solid"
+                  className="py-4 px-4 h-[50px] w-full justify-center"
+                />
+                <ButtonLink
+                  to="/sign-in"
+                  children="Sign In"
+                  variant="outline"
+                  className="py-4 px-4 h-[50px] w-full justify-center"
+                />
+              </div>
+            )}
           </div>
         )}
       </div>
