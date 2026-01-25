99ed97919780c1f61f59b1f9aa8bd7ab052dd271
Yousef Mohsen <ymohsen746@gmail.com>
2026-01-25 03:13:54 +0200

    fix cookies

--- frontend/src/lib/axios.tsx
+++ frontend/src/lib/axios.tsx
@@ -3,23 +3,10 @@ import type { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+    withCredentials: true, // 🔒 Absolute requirement for cookies
     headers: {
         "Content-Type": "application/json"
     }
 });
-
-// Add request interceptor to attach token from localStorage
-axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
 
 export default axiosInstance;
