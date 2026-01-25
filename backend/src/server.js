30a40fa200a38610a4fdb906c632682bab71869e
Yousef Mohsen <ymohsen746@gmail.com>
2026-01-25 02:06:34 +0200

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -86,7 +86,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
     // Handle React routing - return index.html for all non-API routes
-    app.get('*', (req, res) => {
+    app.get('/^\/.*$/', (req, res) => {
         res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
     });
 }
