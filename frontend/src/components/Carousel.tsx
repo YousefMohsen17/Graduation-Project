424504af30b975f55a94013badb0715f61b60504
Yousef Mohsen <ymohsen746@gmail.com>
2026-03-11 11:31:43 +0200

    fill the starts and update quotes in testimonials section

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -33,17 +33,47 @@ const Carousel: React.FC<PropType> = (props) => {
                   <div className="flex gap-3 mb-3.5">
                     <CircleUserRound className="size-14" />
                     <div>
+                      <h4 className="font-bold text-xl mb-1">Yousef Mohsen</h4>
+                      <p>Engineering Student</p>
+                    </div>
+                  </div>
+                  <div className="flex gap-1.5 mb-3.5 items-center">
+                    <div className="flex gap-0.5">
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                    </div>
+                    <h3 className="font-bold text-[25px]">5.0</h3>
+                  </div>
+                </div>
+                <Quote className="size-32 text-[#8490E1]" />
+              </div>
+
+              <p>
+                "The courses here are not just theoretical, they include real practical applications that helped me apply what I learned to my work. Truly an exceptional learning experience!"
+              </p>
+            </div>
+          </div>
+          <div className="embla__slide">
+            <div className="bg-[#ADB5EB] rounded-3xl p-5 ">
+              <div className="flex justify-between">
+                <div>
+                  <div className="flex gap-3 mb-3.5">
+                    <CircleUserRound className="size-14" />
+                    <div>
                       <h4 className="font-bold text-xl mb-1">Fawzi Sayed</h4>
                       <p>Engineering Student</p>
                     </div>
                   </div>
                   <div className="flex gap-1.5 mb-3.5 items-center">
                     <div className="flex gap-0.5">
-                      <Star />
-                      <Star />
-                      <Star />
-                      <Star />
-                      <Star />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
                     </div>
                     <h3 className="font-bold text-[25px]">5.0</h3>
                   </div>
@@ -66,17 +96,17 @@ const Carousel: React.FC<PropType> = (props) => {
                   <div className="flex gap-3 mb-3.5">
                     <CircleUserRound className="size-14" />
                     <div>
-                      <h4 className="font-bold text-xl mb-1">Fawzi Sayed</h4>
+                      <h4 className="font-bold text-xl mb-1">Mostafa Ahmed</h4>
                       <p>Engineering Student</p>
                     </div>
                   </div>
                   <div className="flex gap-1.5 mb-3.5 items-center">
                     <div className="flex gap-0.5">
-                      <Star />
-                      <Star />
-                      <Star />
-                      <Star />
-                      <Star />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
+                      <Star className="fill-yellow-500 text-yellow-500" />
                     </div>
                     <h3 className="font-bold text-[25px]">5.0</h3>
                   </div>
@@ -85,43 +115,7 @@ const Carousel: React.FC<PropType> = (props) => {
               </div>
 
               <p>
-                "This platform is amazing! The courses are well-organized and
-                easy to follow. I felt like I was learning fast without any
-                pressure. I highly recommend it to anyone looking to improve
-                their skills."
-              </p>
-            </div>
-          </div>
-          <div className="embla__slide">
-            <div className="bg-[#ADB5EB] rounded-3xl p-5 ">
-              <div className="flex justify-between">
-                <div>
-                  <div className="flex gap-3 mb-3.5">
-                    <CircleUserRound className="size-14" />
-                    <div>
-                      <h4 className="font-bold text-xl mb-1">Fawzi Sayed</h4>
-                      <p>Engineering Student</p>
-                    </div>
-                  </div>
-                  <div className="flex gap-1.5 mb-3.5 items-center">
-                    <div className="flex gap-0.5">
-                      <Star />
-                      <Star />
-                      <Star />
-                      <Star />
-                      <Star />
-                    </div>
-                    <h3 className="font-bold text-[25px]">5.0</h3>
-                  </div>
-                </div>
-                <Quote className="size-32 text-[#8490E1]" />
-              </div>
-
-              <p>
-                "This platform is amazing! The courses are well-organized and
-                easy to follow. I felt like I was learning fast without any
-                pressure. I highly recommend it to anyone looking to improve
-                their skills."
+                "I've tried many platforms before, but this one really stands out. Support is quick, and the courses are structured professionally. It's a great place to learn and grow."
               </p>
             </div>
           </div>
