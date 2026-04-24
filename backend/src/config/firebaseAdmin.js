0c34060418616eaa209d31e9e3cda948cb383745
Yousef Mohsen <ymohsen746@gmail.com>
2026-04-24 04:22:19 +0300

    use env variable for firebase admin

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -1,8 +1,10 @@
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
   admin.initializeApp({
-    credential: admin.credential.cert(require("../serviceAccountKey.json")),
+    credential: admin.credential.cert(require(serviceAccount)),
   });
 }
 
