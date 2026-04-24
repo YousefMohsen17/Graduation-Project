937a34c7fe9c851363d774280c9a3fd21ef19728
Yousef Mohsen <ymohsen746@gmail.com>
2026-04-24 04:28:30 +0300

    Fix Firebase admin initialization by directly using the service account object instead of requiring it.

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
   admin.initializeApp({
-    credential: admin.credential.cert(require(serviceAccount)),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
