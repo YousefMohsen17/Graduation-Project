7178be7abcad59787aef52041145a6bb3efcfe48
Yousef Mohsen <ymohsen746@gmail.com>
2026-04-30 11:11:49 +0300

    Refactor SignupForm component for improved code clarity and error handling; ensure consistent state management and enhance user feedback on form submission errors.

--- frontend/src/pages/signup/components/SignupForm.tsx
+++ frontend/src/pages/signup/components/SignupForm.tsx
@@ -1,9 +1,8 @@
-
 import Input from "../../../components/Input";
 import ButtonLink from "../../../components/ButtonLink";
 import { Eye, EyeOff, LoaderCircle } from "lucide-react";
 import { useMutation } from "@tanstack/react-query";
-import { createAccount } from "../../../lib/api.tsx"
+import { createAccount } from "../../../lib/api.tsx";
 import { useState } from "react";
 import type { SignupFormData } from "../../../types/types";
 import toast from "react-hot-toast";
@@ -18,7 +17,7 @@ export default function SignupForm() {
     confirmPassword: "",
     department: "",
     academicYear: 0,
-    universityId: ""
+    universityId: "",
   });
   const [errors, setErrors] = useState({
     firstName: "",
@@ -28,38 +27,43 @@ export default function SignupForm() {
     confirmPassword: "",
     department: "",
     academicYear: "",
-    universityId: ""
-  })
-  const [showPassword, setShowPassword] = useState(false)
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
-  const navigate = useNavigate()
+    universityId: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const navigate = useNavigate();
   const { mutate, isPending } = useMutation({
     mutationFn: (data: SignupFormData) => createAccount(data),
     onSuccess: () => {
       navigate("/sign-in", {
         state: {
           showSuccessToast: true,
-        }
+        },
       });
     },
-    onError: (error: any) => {
-      toast.error(error.response?.data.error || "Something went wrong");
+    onError: (error) => {
+      if (error instanceof Error) {
+        toast.error(error.message || "Something went wrong");
+      } else {
+        toast.error("Something went wrong");
+      }
     },
   });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>,
+  ) => {
     const { id, value } = e.target;
     setFormData({
       ...formData,
-      [id]: id === "academicYear" || id === "universityId"
-        ? Number(value)
-        : value
+      [id]:
+        id === "academicYear" || id === "universityId" ? Number(value) : value,
     });
   };
   function handleRegister(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
+    e.preventDefault();
     const result = validatePassword(formData.password);
-
-
 
     setErrors({
       ...errors,
@@ -70,8 +74,8 @@ export default function SignupForm() {
       confirmPassword: "",
       department: "",
       academicYear: "",
-      universityId: ""
-    })
+      universityId: "",
+    });
 
     const newErrors: Record<string, string> = {};
     Object.keys(formData).forEach((key) => {
@@ -80,7 +84,7 @@ export default function SignupForm() {
         newErrors[fieldKey] = `${fieldKey} is required`;
       }
     });
-    setErrors((prev) => ({ ...prev, ...newErrors } as typeof errors));
+    setErrors((prev) => ({ ...prev, ...newErrors }) as typeof errors);
     if (Object.keys(newErrors).length > 0) {
       toast.error("All fields are required");
       return;
@@ -89,16 +93,16 @@ export default function SignupForm() {
       toast.error("Password does not meet all requirements");
       setErrors({
         ...errors,
-        password: "Password does not meet all requirements"
-      })
+        password: "Password does not meet all requirements",
+      });
       return;
     }
     if (formData.password !== formData.confirmPassword) {
       toast.error("Passwords do not match");
       setErrors({
         ...errors,
-        confirmPassword: "Passwords do not match"
-      })
+        confirmPassword: "Passwords do not match",
+      });
       return;
     }
     const data = {
@@ -107,34 +111,49 @@ export default function SignupForm() {
       password: formData.password,
       department: formData.department,
       academicYear: formData.academicYear,
-      universityId: Number(formData.universityId)
-    }
-    mutate(data as SignupFormData)
+      universityId: Number(formData.universityId),
+    };
+    mutate(data as SignupFormData);
   }
   return (
-    <form className="w-full max-w-[643px]" onSubmit={(e) => {
-      handleRegister(e)
-    }}>
+    <form
+      className="w-full max-w-[643px]"
+      onSubmit={(e) => {
+        handleRegister(e);
+      }}
+    >
       <div className="flex flex-col sm:flex-row justify-between mb-4 gap-4 sm:gap-0">
         <div className="flex flex-col w-full sm:w-[48%]">
           <label htmlFor="first-name"> First Name</label>
-          <Input type="text" id="firstName" value={formData.firstName}
+          <Input
+            type="text"
+            id="firstName"
+            value={formData.firstName}
             error={errors.firstName}
-            onChange={handleChange} />
+            onChange={handleChange}
+          />
         </div>
         <div className="flex flex-col w-full sm:w-[48%]">
           <label htmlFor="last-name">Last Name</label>
-          <Input type="text" id="lastName" value={formData.lastName}
+          <Input
+            type="text"
+            id="lastName"
+            value={formData.lastName}
             error={errors.lastName}
-            onChange={handleChange} />
+            onChange={handleChange}
+          />
         </div>
       </div>
 
       <div className="mb-4">
         <label htmlFor="email">Email</label>
-        <Input type="email" id="email" value={formData.email}
+        <Input
+          type="email"
+          id="email"
+          value={formData.email}
           error={errors.email}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
       </div>
       <div className="mb-4">
         <label
@@ -142,15 +161,20 @@ export default function SignupForm() {
           className="flex justify-between mb-2 items-center"
         >
           Password
-          <span className="flex gap-2 font-bold cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
-            {showPassword ?
-              <Eye />
-              : <EyeOff />}
+          <span
+            className="flex gap-2 font-bold cursor-pointer"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <Eye /> : <EyeOff />}
           </span>
         </label>
-        <Input type={showPassword ? "text" : "password"} id="password" value={formData.password}
+        <Input
+          type={showPassword ? "text" : "password"}
+          id="password"
+          value={formData.password}
           error={errors.password}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
         <ul className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-2 list-disc pl-5">
           <li>Use 8 or more characters</li>
           <li>One Uppercase character</li>
@@ -165,23 +189,36 @@ export default function SignupForm() {
           className="flex justify-between mb-2 items-center"
         >
           Confirm Password
-          <span className="flex gap-2 font-bold cursor-pointer" onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
+          <span
+            className="flex gap-2 font-bold cursor-pointer"
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+          >
             {showConfirmPassword ? <Eye /> : <EyeOff />}
           </span>
         </label>
-        <Input type={showConfirmPassword ? "text" : "password"} id="confirmPassword" value={formData.confirmPassword}
+        <Input
+          type={showConfirmPassword ? "text" : "password"}
+          id="confirmPassword"
+          value={formData.confirmPassword}
           error={errors.confirmPassword}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
       </div>
       <div className="mb-4">
         <label htmlFor="department">Department</label>
-        <Input type="text" id="department" value={formData.department}
+        <Input
+          type="text"
+          id="department"
+          value={formData.department}
           error={errors.department}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
       </div>
       <div className="mb-4">
         <label htmlFor="academicYear">Academic Year</label>
-        <select id="academicYear" className={`rounded-xl border ${errors.academicYear ? "border-2 border-red-500 outline-none " : "border border-[#050715] focus-visible:border-black focus-visible:ring-black"} bg-[#EAEDFA] shadow-[4px_4px_4px_4px_rgba(0,0,0,0.25)] py-2 px-4 w-full h-14`}
+        <select
+          id="academicYear"
+          className={`rounded-xl border ${errors.academicYear ? "border-2 border-red-500 outline-none " : "border border-[#050715] focus-visible:border-black focus-visible:ring-black"} bg-[#EAEDFA] shadow-[4px_4px_4px_4px_rgba(0,0,0,0.25)] py-2 px-4 w-full h-14`}
           value={formData.academicYear}
           onChange={handleChange}
         >
@@ -195,20 +232,23 @@ export default function SignupForm() {
       </div>
       <div className="mb-10">
         <label htmlFor="universityId">University ID</label>
-        <Input type="number" id="universityId" value={formData.universityId}
+        <Input
+          type="number"
+          id="universityId"
+          value={formData.universityId}
           error={errors.universityId}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
       </div>
       <ButtonLink
         variant="solid"
         className="py-4 px-5 h-14 w-full flex justify-center"
-        children={isPending ? <LoaderCircle className="animate-spin " /> : "Sign Up"}
+        children={
+          isPending ? <LoaderCircle className="animate-spin " /> : "Sign Up"
+        }
         disabled={isPending}
         type="submit"
       />
-
-
-    </form >);
+    </form>
+  );
 }
-
-
