11428257d521c59ae65cd76ea9b9bd33cf71b211
Yousef Mohsen <ymohsen746@gmail.com>
2026-04-30 11:11:10 +0300

    Refactor GlassButton and CourseBox components for improved readability and maintainability; update class names for consistency in styling.

--- frontend/src/components/GlassButton.tsx
+++ frontend/src/components/GlassButton.tsx
@@ -1,10 +1,23 @@
-export default function GlassButton({ children, className, onClick, key, disabled }: { children: React.ReactNode, className?: string, onClick?: () => void, key?: string, disabled?: boolean }) {
-    return (
-        <button className={`
+export default function GlassButton({
+  children,
+  className,
+  onClick,
+  key,
+  disabled,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+  key?: string;
+  disabled?: boolean;
+}) {
+  return (
+    <button
+      className={`
         mt-4  
         flex items-center justify-center gap-[8px]
         px-[24px] py-[16px] rounded-[16px]
-        bg-gradient-to-b from-[#d0d5f3] to-[#ced2f2]
+        bg-linear-to-b from-[#d0d5f3] to-[#ced2f2]
         backdrop-blur-2xl
         shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]
         text-slate-800 font-medium transition-all active:scale-95
@@ -12,11 +25,11 @@ export default function GlassButton({ children, className, onClick, key, disable
     ${className}
         ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer active:scale-95"}
       `}
-            onClick={onClick}
-            key={key}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    )
+      onClick={onClick}
+      key={key}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 }
